72fbc547d77fb1da5bb2dcf826cd9a8b9e17dee8
Your Name <newuser@example.com>
2016-10-07 16:20:38 +0700

    Admin User

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,19 @@
+class Admin::UsersController < ApplicationController
+  before_action :logged_in_user
+  before_action :verify_admin
+  before_action :load_user, only: :destroy
+
+  def index
+    @users = User.recent.paginate page: params[:page], per_page: Settings.per_page
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "message.delete_success"
+      redirect_to admin_users_path
+    else
+      flash[:warning] = t "message.delete_fail"
+      redirect_to root_path
+    end
+  end
+end
