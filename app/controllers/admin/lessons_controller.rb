05dc71eda153ccf8bfd468e7ca3dfd66b2091fd8
Your Name <newuser@example.com>
2016-10-11 08:29:17 +0700

    Admin Lessons

---
+++ app/controllers/admin/lessons_controller.rb
@@ -0,0 +1,29 @@
+class Admin::LessonsController < ApplicationController
+  before_action :logged_in_user
+  before_action :verify_admin
+  before_action :load_lesson, only: :destroy
+
+  def index
+    @lessons = Lesson.recent.includes(:user)
+      .paginate page: params[:page], per_page: Settings.per_page
+  end
+
+  def destroy
+    if @lesson.destroy
+      flash[:success] = t "lesson.delete"
+    else
+      flash[:danger] = t "lesson.delete_fail"
+    end
+    redirect_to admin_lessons_path
+  end
+
+  private
+  
+  def load_lesson
+    @lesson = Lesson.find_by id: params[:id]
+    if @lesson.nil?
+      flash[:danger] = t "lesson.not_found"
+      redirect_to admin_lessons_path
+    end
+  end
+end
